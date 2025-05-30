5486a6ecf976fbcf552908a1e0f4bb3fe3f6c512
Federico Capaldo <federico.capaldo@hotmail.com>
2025-05-30 13:59:31 +0200

    generating photo for picture with ai api request, saving that to cloudinary with photo.attach method called on model, and rendering that with a cloudinary image tag helper method in the view of _recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,12 @@
+require "open-uri"
 class Recipe < ApplicationRecord
+  has_one_attached :photo
 
-  after_save :set_content, if: -> { saved_change_to_name? || saved_change_to_ingredients? }
+  after_save if: -> { saved_change_to_name? || saved_change_to_ingredients? } do
+    set_content
+    set_photo
+  end
+
 
   def content
     if super.blank?
@@ -26,5 +32,19 @@ class Recipe < ApplicationRecord
 
     self.update(content: new_content)
     return new_content
+  end
+
+  def set_photo
+    client = OpenAI::Client.new
+    chatgpt_response = client.images.generate(parameters: {
+      prompt: "A recipe image of #{self.name}", size: "256x256"
+    })
+
+    new_image_url = chatgpt_response["data"][0]["url"]
+    new_image_file =  URI.parse(new_image_url).open
+
+    photo.purge if photo.attached?
+    photo.attach(io: new_image_file, filename: "ai_generated_image.png", content_type: "image/png")
+    return photo
   end
 end
