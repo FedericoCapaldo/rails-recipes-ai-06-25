d13afd938adb6841ed115d3a680b233dc578cbf5
Federico Capaldo <federico.capaldo@hotmail.com>
2025-05-30 13:18:23 +0200

    created content field on recipe in db, so as to permanently save generated recipe content and return always that.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,17 +1,28 @@
 class Recipe < ApplicationRecord
 
-
   def content
-    Rails.cache.fetch("#{cache_key_with_version}/content") do
-      client = OpenAI::Client.new
-      chatgpt_response = client.chat(parameters: {
-        "model": "gpt-4o-mini",
-        messages: [{
-          role: "user",
-          content: "Give me a simple recipe for #{self.name} with the ingredients #{self.ingredients}. Give me only the text of the recipe, without any of your own answer like 'Here is a simple recipe'."
-        }]
-      })
-      chatgpt_response["choices"][0]["message"]["content"]
+    if super.blank?
+      set_content
+    else
+      super
     end
+  end
+
+
+  private
+
+  def set_content
+    client = OpenAI::Client.new
+    chatgpt_response = client.chat(parameters: {
+      "model": "gpt-4o-mini",
+      messages: [{
+        role: "user",
+        content: "Give me a simple recipe for #{self.name} with the ingredients #{self.ingredients}. Give me only the text of the recipe, without any of your own answer like 'Here is a simple recipe'."
+      }]
+    })
+    new_content = chatgpt_response["choices"][0]["message"]["content"]
+
+    self.update(content: new_content)
+    return new_content
   end
 end
