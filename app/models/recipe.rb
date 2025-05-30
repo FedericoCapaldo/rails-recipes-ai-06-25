7234f015d8ac4d2c000fffe362f6e40d11836c55
Federico Capaldo <federico.capaldo@hotmail.com>
2025-05-30 13:13:34 +0200

    added cache to function so as to save content in rails memory cache (this is temporary storage!) and avoid a new recepie generation on each reload

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,14 +2,16 @@ class Recipe < ApplicationRecord
 
 
   def content
-    client = OpenAI::Client.new
-    chatgpt_response = client.chat(parameters: {
-      "model": "gpt-4o-mini",
-      messages: [{
-        role: "user",
-        content: "Give me a simple recipe for #{self.name} with the ingredients #{self.ingredients}. Give me only the text of the recipe, without any of your own answer like 'Here is a simple recipe'."
-      }]
-    })
-    return chatgpt_response["choices"][0]["message"]["content"]
+    Rails.cache.fetch("#{cache_key_with_version}/content") do
+      client = OpenAI::Client.new
+      chatgpt_response = client.chat(parameters: {
+        "model": "gpt-4o-mini",
+        messages: [{
+          role: "user",
+          content: "Give me a simple recipe for #{self.name} with the ingredients #{self.ingredients}. Give me only the text of the recipe, without any of your own answer like 'Here is a simple recipe'."
+        }]
+      })
+      chatgpt_response["choices"][0]["message"]["content"]
+    end
   end
 end
