4fc403748198fc0030334a4ab3e3412bf746741f
Federico Capaldo <federico.capaldo@hotmail.com>
2025-06-04 20:51:13 +0100

    moved creation of receipe to a job that can be run in the background after name and ingredients are saved

---
+++ app/jobs/recipe_content_job.rb
@@ -0,0 +1,18 @@
+class RecipeContentJob < ApplicationJob
+  queue_as :default
+
+  def perform(recipe)
+    puts "Generating recipe content with AI background job"
+    client = OpenAI::Client.new
+    chatgpt_response = client.chat(parameters: {
+      "model": "gpt-4o-mini",
+      messages: [{
+        role: "user",
+        content: "Give me a simple recipe for #{recipe.name} with the ingredients #{recipe.ingredients}. Give me only the text of the recipe, without any of your own answer like 'Here is a simple recipe'."
+      }]
+    })
+    new_content = chatgpt_response["choices"][0]["message"]["content"]
+
+    recipe.update(content: new_content)
+  end
+end
